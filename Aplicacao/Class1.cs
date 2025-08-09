5080842c2a805cf46ce3d24ad4abbfaaa1451d25
viniciushiagorosa <viniciushiagor@gmail.com>
2025-08-09 19:19:35 -0300

    feat: criado os controller e as regras de negócio junto com o acesso ao banco de dados

--- Aplicacao/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Aplicacao
-{
-    public class Class1
-    {
-
-    }
-}
--- Categorias/Program.cs
+++ Categorias/Program.cs
@@ -1,3 +1,10 @@
+using Aplicacao.Interfaces;
+using Aplicacao.Services;
+using Infraestrutura;
+using Infraestrutura.Interfaces;
+using Infraestrutura.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +13,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<ICategoriaRepository, CategoriaRepository>();
+builder.Services.AddScoped<ICategoriaService, CategoriaService>();
 
 var app = builder.Build();
 
