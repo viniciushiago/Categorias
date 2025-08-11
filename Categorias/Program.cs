866e69f9866269368536c3514d6e6c23a60f5a88
viniciushiagorosa <viniciushiagor@gmail.com>
2025-08-11 17:50:49 -0300

    feat:ajustado launch para o yarp encontrar

--- Categorias/Program.cs
+++ Categorias/Program.cs
@@ -22,12 +22,8 @@ builder.Services.AddScoped<ICategoriaService, CategoriaService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
